f0bf2238e3cb108095fe6d9b14a2b553a3dc4190
Juntab1 <jsbwhaleguy@gmail.com>
2024-06-19 22:26:28 -0700

    what to do next

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+// write functions to insert, find, and delete itesm from it
+// look at dynamic memory again
+
 typedef struct node {
-    int val;
+    // using char* makes it dynamically allocated to the heap
+    char* str;
     // struct is needed here since node is not yet created
     struct node* next;
     struct node* prev;
